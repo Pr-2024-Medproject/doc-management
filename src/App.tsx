de00bb2f62222154490a773d4596b82ed5f446eb
halmber <rogozyanskiy.bogdan@gmail.com>
2024-05-22 16:18:36 +0300

    added prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <h1 className="text-blue-500 text-center text-2xl">Clinic documentation management</h1>
-    </>
-  );
+    return (
+        <>
+            <h1 className="text-blue-500 text-center text-2xl">Clinic documentation management</h1>
+        </>
+    );
 };
 
 export default App;
