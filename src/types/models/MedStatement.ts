3b612d336670ec41e251800e0d2356166211e711
halmber <rogozyanskiy.bogdan@gmail.com>
2024-05-24 22:40:10 +0300

    feat: patientData interface

--- src/types/models/MedStatement.ts
+++ src/types/models/MedStatement.ts
@@ -1,23 +1,14 @@
 import { PatientData } from "./Patient";
 
 export interface MedStatementModel extends PatientData {
-    address: string;
-    job: string;
-    hospitalizationDate: Date;
-    dischargeDate: Date;
-    mainDiagnosis: string;
-    complicationsDiagnosis: string;
     relatedDiagnosis: string;
-    complaints: string;
     medicalHistory: string;
     KT: string;
     MRT: string;
     FBC: string;
     FEGDC: string;
-    histology: string;
     MDKDecision: string;
     PT_PHTOperation: string;
     recommendations: string;
     date: Date;
-    doctor: string;
 }
