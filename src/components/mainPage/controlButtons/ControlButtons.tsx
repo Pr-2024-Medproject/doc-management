049774e2a6fcd1b85f1106793eb0bf2b2298467a
daniilraptanov <daniilraptanov@gmail.com>
2024-05-29 17:27:22 +0300

    fix: update of NewPatientButton handler

--- src/components/mainPage/controlButtons/ControlButtons.tsx
+++ src/components/mainPage/controlButtons/ControlButtons.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import Input from "../../shared/Input";
 import { useStore } from "../../../store";
+import { useNavigate } from "react-router-dom";
+import { getPatientDataFormInfo } from "../../../services/FormServiceImpl";
 
 interface ControlButtonsProps {
     disabled: boolean;
@@ -9,7 +11,14 @@ interface ControlButtonsProps {
 }
 
 const ControlButtons: FC<ControlButtonsProps> = (props) => {
+    const navigate = useNavigate();
+    const formInfo = getPatientDataFormInfo();
     const { selectedPatient, setSelectedPatient } = useStore();
+
+    const createNewPatientHandler = () => {
+        setSelectedPatient(null);
+        navigate(`/doc-management/form/${formInfo?.id}`);
+    };
 
     return (
         <div className="flex justify-end p-4 pr-20 justify-self-end">
@@ -19,7 +28,7 @@ const ControlButtons: FC<ControlButtonsProps> = (props) => {
                         type="button"
                         value="Or create new patient"
                         disabled={false}
-                        onClick={() => setSelectedPatient(null)}
+                        onClick={createNewPatientHandler}
                     />
                 )}
             </div>
