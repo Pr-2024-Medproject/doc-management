da6dccc35527b4e8a6957c8145251bbc0d122872
daniilraptanov <daniilraptanov@gmail.com>
2024-05-29 18:18:16 +0300

    feat: add button for navigate to main page

--- src/components/mainPage/navigationPanel/NavigationPanel.tsx
+++ src/components/mainPage/navigationPanel/NavigationPanel.tsx
@@ -1,15 +1,30 @@
 import { FC } from "react";
 import NavigationItem from "./NavigationItem";
 import { FORMS } from "../../../constants/Forms";
+import Input from "../../shared/Input";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface NavigationPanelProps {}
 
 const NavigationPanel: FC<NavigationPanelProps> = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const isMainPage = location.pathname === "/doc-management/";
+
     return (
         <nav className="h-full p-4 flex justify-evenly">
-            {FORMS.map((info) => <NavigationItem key={info.key} formInfo={info} />)}
+            {!isMainPage && (
+                <Input
+                    type="button"
+                    value={"Перейти на головну"}
+                    onClick={() => navigate("/doc-management/")}
+                />
+            )}
+            {FORMS.map((info) => (
+                <NavigationItem key={info.key} formInfo={info} />
+            ))}
         </nav>
     );
-}
+};
 
 export default NavigationPanel;
