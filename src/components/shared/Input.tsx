82a36fb4be5861b64f288d91db5a8a23d504a5ed
daniilraptanov <daniilraptanov@gmail.com>
2024-05-31 16:25:24 +0300

    fix: inputs errors

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -50,7 +50,7 @@ const Input: FC<InputProps> = ({
                 className={`px-3 py-2 text-base text-[#344054] bg-white border border-[#d0d5dd] shadow-sm rounded-lg placeholder-[#667085] ${buttonStyle} ${className}`}
             />
             <p className={`mt-1 ml-3 text-sm font-normal text-red-500`}>
-                {error || name ? "\u00A0" : ""}
+                {error || (name ? "\u00A0" : "")}
             </p>
         </div>
     );
